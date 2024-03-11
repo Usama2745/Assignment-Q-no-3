25735b58f59b47a21c5b9ab084fe21880489fe42
Usama <nadeemu919@gmail.com>
2024-03-12 00:17:47 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Assignment-Q-no-3" 
---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+// store a persone name in a variable, and then print that person name in the lowercase,
+// uppercase.and titlecase.
+let firstName = "Usama";
+let lastname = "Nadeem";
+let fullname = firstName + "" + lastname;
+console.log(fullname);
+// step1 person name print the lowercase.
+let step1 = fullname.toLowerCase();
+console.log(step1);
+// step 2; person name print the uppercase.
+let step2 = fullname.toUpperCase();
+console.log(step2);
+// step 3; person name print the titlecase.
+let step3 = fullname.toString();
+console.log(step3);
